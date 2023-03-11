d3e0283ea62941aee678575b430829704e96f481
haritinkus <88307120+haritinkus@users.noreply.github.com>
2023-03-11 05:39:12 +0530

    first commit

---
+++ src/main/java/firstdemo/FirstDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package firstdemo;
+
+/**
+ * @author Harish
+ *
+ */
+public class FirstDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Demo");
+	}
+
+}
